d7db39d358ad6664bf6eae6e6ee9ec02e10b65ee
Lindsay Lopez <lopez95@berkeley.edu>
2021-08-09 18:32:30 -0700

    starting tests/implementation for ship.js

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,2 +1,7 @@
 /* eslint-disable no-undef */
 const ship = require('../code/ship');
+const shipOfLength3 = ship(3);
+
+test('entering number less than 0 does not hit (returns false)', () => {
+  expect(shipOfLength3.hit(-1)).toBeFalsy();
+});
