e15a39f2e871b8176aabd202521c4cdc02dd7b14
Lindsay Lopez <lopez95@berkeley.edu>
2021-08-09 19:39:34 -0700

    finished testing/implementation for Ship

--- code/ship.js
+++ code/ship.js
@@ -1,4 +1,23 @@
-const ship = () => {};
+const Ship = (length) => {
+  const spotsHit = Array(length).fill(0);
+
+  const hit = (spot) => {
+    if (spot >= 0 && spot < length && !spotsHit[spot]) {
+      spotsHit[spot] = 1;
+      return true;
+    }
+    return false;
+  };
+
+  const isSunk = () => {
+    return spotsHit.reduce((sum, current) => sum + current, 0) === length;
+  };
+
+  return {
+    hit,
+    isSunk,
+  };
+};
 
 // eslint-disable-next-line no-undef
-module.exports = ship;
+module.exports = Ship;
--- test/ship.test.js
+++ test/ship.test.js
@@ -1,7 +1,47 @@
 /* eslint-disable no-undef */
-const ship = require('../code/ship');
-const shipOfLength3 = ship(3);
+const Ship = require('../code/ship');
 
-test('entering number less than 0 does not hit (returns false)', () => {
+// testing "hit" function
+const shipOfLength3 = Ship(3);
+
+test('hit: entering a number less than 0 does not hit (returns false)', () => {
   expect(shipOfLength3.hit(-1)).toBeFalsy();
+});
+
+test('hit: entering a valid number that has not been hit returns true', () => {
+  expect(shipOfLength3.hit(1)).toBeTruthy();
+});
+
+test('hit: entering a valid number that has been hit returns false', () => {
+  expect(shipOfLength3.hit(1)).toBeFalsy();
+});
+
+test('hit: entering a number greater than the length does not hit (returns false)', () => {
+  expect(shipOfLength3.hit(3)).toBeFalsy();
+});
+
+//testing "isSunk" function
+const shipOfLength1 = Ship(1);
+
+test('isSunk: empty ship should return false', () => {
+  expect(shipOfLength1.isSunk()).toBeFalsy();
+});
+
+test('isSunk: full ship should return true', () => {
+  shipOfLength1.hit(0);
+  expect(shipOfLength1.isSunk()).toBeTruthy();
+});
+
+test('isSunk: non-empty but not full ship retuns false', () => {
+  expect(shipOfLength3.isSunk()).toBeFalsy();
+});
+
+test('isSunk: non-empty but not full ship retuns false', () => {
+  shipOfLength3.hit(0);
+  expect(shipOfLength3.isSunk()).toBeFalsy();
+});
+
+test('isSunk: full ship should return true', () => {
+  shipOfLength3.hit(2);
+  expect(shipOfLength3.isSunk()).toBeTruthy();
 });
