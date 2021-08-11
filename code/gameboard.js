d8a7c860443b3611419f0a907fd3ee8ae7c7ebbb
Lindsay Lopez <lopez95@berkeley.edu>
2021-08-10 21:36:55 -0700

    working on canPlaceShip logic

--- code/gameboard.js
+++ code/gameboard.js
@@ -1,6 +1,44 @@
+const Ship = require('./ship');
+
 const Gameboard = () => {
-  const placeShip = () => {
-    return false;
+  const gameboardSize = 10;
+  const positions = Array(10).fill(Array(10).fill({ hit: false }));
+
+  const canPlaceShip = (xStart, yStart, shipLength, shipOrientation) => {
+    if (
+      xStart < 0 ||
+      xStart >= gameboardSize ||
+      yStart < 0 ||
+      yStart >= gameboardSize
+    ) {
+      return false;
+    }
+
+    let xEnd =
+      shipOrientation === 'horizontal' ? xStart + shipLength : xStart + 1;
+    let yEnd =
+      shipOrientation === 'vertical' ? yStart + shipLength : yStart + 1;
+
+    for (let x = xStart; x < xEnd; x = x + 1) {
+      for (let y = yStart; y < yEnd; y = y + 1) {
+        
+      }
+    }
+  };
+
+  const placeShip = (xStart, yStart, shipLength, shipOrientation) => {
+    if (!canPlaceShip) {
+      return false;
+    }
+    const newShip = Ship(shipLength);
+    for (
+      let positionsX = x;
+      positionsX < x + shipOrientation;
+      positionsX = positionsX + 1
+    ) {
+      positions[positionsX][y] = newShip;
+    }
+    return true;
   };
 
   return {
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,11 +1,36 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 const Gameboard = require('../code/gameboard');
 
 const gameboard = Gameboard();
+const gameboardSize = 10;
 
 // testing placeShip
-// eslint-disable-next-line no-undef
-test('placeShip: placing a ship outside of the gamboard returns false', () => {
-  // eslint-disable-next-line no-undef
-  expect(gameboard.placeShip(-1, -1, 3)).toBeFalsy();
+test('placeShip: placing a ship outside of the gamboard returns false test 1', () => {
+  expect(gameboard.placeShip(-1, -1, 3, 'horizontal')).toBeFalsy();
 });
+
+test('placeShip: placing a ship outside of the gamboard returns false test 2', () => {
+  expect(gameboard.placeShip(-1, gameboardSize, 3, 'vertical')).toBeFalsy();
+});
+
+test('placeShip: placing a ship outside of the gamboard returns false test 3', () => {
+  expect(gameboard.placeShip(gameboardSize, -3, 3, 'horizontal')).toBeFalsy();
+});
+
+test('placeShip: placing a ship outside of the gamboard returns false test 3', () => {
+  expect(gameboard.placeShip(gameboardSize + 10, gameboardSize, 3, 'vertical')).toBeFalsy();
+});
+
+test('placeShip: placing a ship inside of the gamboard on a valid place returns true', () => {
+  expect(gameboard.placeShip(0, 0, 3, 'horizontal')).toBeTruthy();
+});
+
+test('placeShip: placing a ship inside of the gamboard on a valid place returns true', () => {
+  expect(gameboard.placeShip(5, 6, 4, 'horizontal')).toBeTruthy();
+});
+
+test('placeShip: placing a ship inside of the gamboard on a spot already occupied returns false', () => {
+  expect(gameboard.placeShip(7, 4, 4, 'vertical')).toBeFalsy();
+});
+
+
