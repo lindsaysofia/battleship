4dc2e525f6e3899905e1d92d6e514b92780cbfde
Lindsay Lopez <lopez95@berkeley.edu>
2021-08-11 18:13:28 -0700

    finished testing/implementing placeShip

--- code/gameboard.js
+++ code/gameboard.js
@@ -1,42 +1,49 @@
+/* eslint-disable no-undef */
 const Ship = require('./ship');
 
 const Gameboard = () => {
   const gameboardSize = 10;
-  const positions = Array(10).fill(Array(10).fill({ hit: false }));
+  const positions = Array(10).fill(Array.from({length: 10}, () => ({hit: false})));
 
-  const canPlaceShip = (xStart, yStart, shipLength, shipOrientation) => {
+  const isValidShipPlacement = (xStart, yStart, xEnd, yEnd) => {
+    // if ship is outside gameboard
     if (
       xStart < 0 ||
-      xStart >= gameboardSize ||
+      xEnd < 0 ||
       yStart < 0 ||
-      yStart >= gameboardSize
+      yEnd < 0 ||
+      xStart >= gameboardSize ||
+      xEnd >= gameboardSize ||
+      yStart >= gameboardSize ||
+      yEnd >= gameboardSize
     ) {
       return false;
     }
 
-    let xEnd =
-      shipOrientation === 'horizontal' ? xStart + shipLength : xStart + 1;
-    let yEnd =
-      shipOrientation === 'vertical' ? yStart + shipLength : yStart + 1;
-
-    for (let x = xStart; x < xEnd; x = x + 1) {
-      for (let y = yStart; y < yEnd; y = y + 1) {
-        
+    // if there is already a ship at the spot
+    for (let x = xStart; x < xEnd; x++) {
+      for (let y = yStart; y < yEnd; y++) {
+        if (positions[x][y].ship) {
+          return false;
+        }
       }
     }
+    return true;
   };
 
   const placeShip = (xStart, yStart, shipLength, shipOrientation) => {
-    if (!canPlaceShip) {
+    const xEnd =
+      shipOrientation === 'horizontal' ? xStart + shipLength : xStart + 1;
+    const yEnd =
+      shipOrientation === 'vertical' ? yStart + shipLength : yStart + 1;
+    if (!isValidShipPlacement(xStart, yStart, xEnd, yEnd)) {
       return false;
     }
     const newShip = Ship(shipLength);
-    for (
-      let positionsX = x;
-      positionsX < x + shipOrientation;
-      positionsX = positionsX + 1
-    ) {
-      positions[positionsX][y] = newShip;
+    for (let x = xStart; x < xEnd; x++) {
+      for (let y = yStart; y < yEnd; y++) {
+        positions[x][y].ship = newShip;
+      }
     }
     return true;
   };
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -17,20 +17,32 @@ test('placeShip: placing a ship outside of the gamboard returns false test 3', (
   expect(gameboard.placeShip(gameboardSize, -3, 3, 'horizontal')).toBeFalsy();
 });
 
-test('placeShip: placing a ship outside of the gamboard returns false test 3', () => {
+test('placeShip: placing a ship outside of the gamboard returns false test 4', () => {
   expect(gameboard.placeShip(gameboardSize + 10, gameboardSize, 3, 'vertical')).toBeFalsy();
 });
 
-test('placeShip: placing a ship inside of the gamboard on a valid place returns true', () => {
+test('placeShip: placing a ship outside of the gamboard returns false test 5', () => {
+  expect(gameboard.placeShip(6, 0, 5, 'horizontal')).toBeFalsy();
+});
+
+test('placeShip: placing a ship inside of the gamboard on a valid place returns true test 1', () => {
   expect(gameboard.placeShip(0, 0, 3, 'horizontal')).toBeTruthy();
 });
 
-test('placeShip: placing a ship inside of the gamboard on a valid place returns true', () => {
+test('placeShip: placing a ship inside of the gamboard on a valid place returns true test 2', () => {
   expect(gameboard.placeShip(5, 6, 4, 'horizontal')).toBeTruthy();
 });
 
-test('placeShip: placing a ship inside of the gamboard on a spot already occupied returns false', () => {
+test('placeShip: placing a ship inside of the gamboard on a valid place returns true test 3', () => {
+  expect(gameboard.placeShip(2, 1, 5, 'vertical')).toBeTruthy();
+});
+
+test('placeShip: placing a ship inside of the gamboard on a spot already occupied returns false test 1', () => {
   expect(gameboard.placeShip(7, 4, 4, 'vertical')).toBeFalsy();
+});
+
+test('placeShip: placing a ship inside of the gamboard on a spot already occupied returns false test 2', () => {
+  expect(gameboard.placeShip(0, 0, 3, 'horizontal')).toBeFalsy();
 });
 
 
