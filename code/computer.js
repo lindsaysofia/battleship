e8d82ba261f3a70cbb41dacef65c816d52daec85
Lindsay Lopez <lopez95@berkeley.edu>
2021-08-13 19:55:25 -0700

    finished player and computer

--- code/computer.js
+++ code/computer.js
@@ -1,11 +1,33 @@
+/* eslint-disable no-undef */
 const Player = require('./player');
 
 const Computer = () => {
+  const enemyPositionsAttacked = [];
 
-  const {getName, getGameboard} = Player('Computer');
+  const gameboardSize = 10;
+
+  const { getName, getGameboard, isValidAttack } = Player('Computer');
+
+  const getRandomCoordinate = () => {
+    return Math.floor(Math.random * gameboardSize);
+  };
+
+  const attack = () => {
+    let x = getRandomCoordinate();
+    let y = getRandomCoordinate();
+    while (!isValidAttack(x, y)) {
+      x = getRandomCoordinate();
+      y = getRandomCoordinate();
+    }
+    enemyPositionsAttacked.push({ x, y });
+    return { x, y };
+  };
 
   return {
     getName,
     getGameboard,
+    attack,
   };
 };
+
+module.exports = Computer;
--- code/player.js
+++ code/player.js
@@ -2,17 +2,37 @@
 const Gameboard = require('./gameboard');
 
 const Player = (name) => {
-    const gameboard = Gameboard();
-    const enemyPositionsAttacked = [];
+  const enemyPositionsAttacked = [];
+  const gameboard = Gameboard();
 
-    const getName = () => name;
+  const getName = () => name;
 
-    const getGameboard = () => gameboard;
+  const getGameboard = () => gameboard;
 
-    return {
-      getName,
-      getGameboard
-    };
+  const isValidAttack = (x, y) => {
+    for (let i = 0; i < enemyPositionsAttacked.length; i++) {
+      let enemyPositions = enemyPositionsAttacked[i];
+      if (enemyPositions.x === x && enemyPositions.y === y) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const attack = (x, y) => {
+    if (!isValidAttack(x, y)) {
+      return false;
+    }
+    enemyPositionsAttacked.push({ x, y });
+    return true;
+  };
+
+  return {
+    getName,
+    getGameboard,
+    isValidAttack,
+    attack,
+  };
 };
 
 module.exports = Player;
