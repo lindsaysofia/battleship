a4a8ab03b25ee22c15d23e4d63aa39c52f733e3e
Lindsay Lopez <lopez95@berkeley.edu>
2021-08-15 19:57:47 -0700

    initiating variables

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
-console.log('woo');
+const Player = require('./code/player');
+const Computer = require('./code/computer');
+const DOMLogic = require('./code/domLogic');
+
+let isPlayerTurn;
+let isPlayerSunk;
+let isComputerSunk;
+let player;
+let computer;
+
+function initiateGame() {
+  isPlayerTurn = true;
+  isPlayerSunk = false;
+  isComputerSunk = false;
+  player = Player('Linda');
+  computer = Computer();
+}
+
+initiateGame();
